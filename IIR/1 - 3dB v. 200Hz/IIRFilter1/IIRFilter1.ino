b25050203aff82e6eb5eedcc67bfe3a7e4c978b8
JRIX <43089857+JOCRIX@users.noreply.github.com>
2024-02-06 17:36:44 +0100

    lol

--- IIR/1 - 3dB v. 200Hz/IIRFilter1/IIRFilter1.ino
+++ IIR/1 - 3dB v. 200Hz/IIRFilter1/IIRFilter1.ino
@@ -50,6 +50,8 @@ void setup() {
 }
 
 void loop() {
+
+  
   unsigned long currentMicros = micros();
 
 
@@ -63,7 +65,7 @@ void loop() {
 
     //Serial.println(y0fil);
     SetOutput(y0fil);
-
+    previousMicros = currentMicros;
     /*
     if (state == 0) {
       test1 = currentMicros;
@@ -81,18 +83,22 @@ void loop() {
     }
 */
   }
+ 
 }
 
 void SetOutput(float filterOut) {
 
-  uint16_t outputVal = round(filterOut);
+  uint16_t outputVal = round(filterOut * 0.25);
 
   uint8_t outVals[10];
 
 
   for (uint8_t i = 0; i < 10; i++) {
     outVals[i] = outputVal & (1 << i);
+  ////  Serial.print(outVals[i]);
   }
+  //Serial.println();
+
 
   for (uint8_t j = 0; j < 10; j++) {
     digitalWrite(outArr[j], outVals[j]);
